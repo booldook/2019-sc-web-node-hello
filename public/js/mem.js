49c4033142810ced213ef7db798ab5a256b2c6bd
booldook <booldook@gmail.com>
2019-10-10 13:44:41 +0900

    회원 아이디 중복 체크

--- public/js/mem.js
+++ public/js/mem.js
@@ -1,18 +1,25 @@
 // 아이디 중복 체크
-$("#userid").on("blur", function(){
+$("#userid").on("blur", function () {
 	var userid = $(this).val();
-	ajax("/api-mem/userid", "post", {userid: userid}, function(res){
-		$(".userid-cmt").empty();
-		if(res.chk) {
-			$(".userid-cmt").text('* 사용가능한 아이디 입니다.');
-			$(".userid-cmt").css({"color": "blue"});
-			$("#userid").css({"border": "1px solid blue"});
-		}
-		else {
-			$(".userid-cmt").text('* 사용할 수 없는 아이디 입니다.');
-			$(".userid-cmt").css({"color": "red"});
-			$("#userid").css({"border": "1px solid red"});
-			$("#userid").focus();
-		}
-	});
+	$(".userid-cmt").empty();
+	if (userid.length >= 8 && userid.length <= 16) {
+		ajax("/api-mem/userid", "post", {userid: userid}, function (res) {
+			if (res.chk) {
+				$(".userid-cmt").text('* 사용가능한 아이디 입니다.');
+				$(".userid-cmt").css({"color": "blue"});
+				$("#userid").css({"border": "1px solid blue"});
+			} else {
+				$(".userid-cmt").text('* 사용할 수 없는 아이디 입니다.');
+				$(".userid-cmt").css({"color": "red"});
+				$("#userid").css({"border": "1px solid red"});
+				$("#userid").focus();
+			}
+		});
+	}
+	else {
+		$(".userid-cmt").text("* 아이디는 8 ~ 16자 입니다.");
+		$(".userid-cmt").css({"color": "red"});
+		$("#userid").css({"border": "1px solid red"});
+		$("#userid").focus();
+	}
 });
