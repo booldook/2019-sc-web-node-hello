cf4e5b7f5158673b7395d89a1f1b89fe970c32b3
booldook <booldook@gmail.com>
2019-10-14 10:50:10 +0900

    navi.pug

---
+++ public/js/navi.js
@@ -0,0 +1,6 @@
+$(".mem-edit").parent().mouseenter(function(){
+	$(".mem-edit").stop().slideDown(300);
+});
+$(".mem-edit").parent().mouseleave(function(){
+	$(".mem-edit").stop().slideUp(300);
+});
